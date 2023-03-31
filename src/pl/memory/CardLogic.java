09435a115ed8b4013e272c1d2236cab9188fe4a1
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-03-31 22:47:16 +0200

    added enum for card position

--- src/pl/memory/CardLogic.java
+++ src/pl/memory/CardLogic.java
@@ -4,11 +4,17 @@ import javax.swing.*;
 import java.io.Serializable;
 
 public class CardLogic implements Serializable {
+
+    private enum CardPosition {
+        REVERSE, AVERSE
+    }
+
     String name;
     ImageIcon reverse;
     ImageIcon observe;
     ImageIcon activeIcon;
     boolean guessed = false;
+    private CardPosition averse;
 
     public CardLogic() {
     }
