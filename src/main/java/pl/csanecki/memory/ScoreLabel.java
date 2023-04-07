c414dea268e1b5695d06772a8fee5d0c68ca1263
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-04-07 15:42:49 +0200

    fixed label

--- src/main/java/pl/csanecki/memory/ScoreLabel.java
+++ src/main/java/pl/csanecki/memory/ScoreLabel.java
@@ -8,17 +8,13 @@ import java.time.Duration;
 public class ScoreLabel extends JLabel implements Subscriber {
 
     public ScoreLabel(int width) {
-        super("Time Score: " + 0 + " s");
+        super("Let's start the game!");
         setHorizontalAlignment(SwingConstants.CENTER);
         setBounds(0, 0, width, 40);
     }
 
-    public void updateSeconds(int seconds) {
-        setText("Time Score: " + (double) (seconds) / 10 + " s");
-    }
-
     @Override
     public void update(Duration passed) {
-        updateSeconds(passed.toSecondsPart());
+        setText(String.format("Timer: %d.%03ds", passed.toSecondsPart(), passed.toMillisPart()));
     }
 }
