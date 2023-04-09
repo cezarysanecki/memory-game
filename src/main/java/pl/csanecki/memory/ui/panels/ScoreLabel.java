cce10585af3d157054f01de7158887b4d473c1f3
Cezary Sanecki <saneckicezary@gmail.com>
2023-04-09 23:18:26 +0200

    fixed stupid bug

--- src/main/java/pl/csanecki/memory/ui/panels/ScoreLabel.java
+++ src/main/java/pl/csanecki/memory/ui/panels/ScoreLabel.java
@@ -19,9 +19,9 @@ public class ScoreLabel extends JLabel implements MillisTimerSubscriber {
     @Override
     public void update(Duration passed, MillisRefreshment millisRefreshment) {
         String scoreText = switch (millisRefreshment) {
-            case OneThousand -> String.format("Timer: %ds", passed.toSecondsPart());
-            case OneHundred -> String.format("Timer: %d.%ds", passed.toSecondsPart(), passed.toMillisPart() / 100);
-            case Ten -> String.format("Timer: %d.%02ds", passed.toSecondsPart(), passed.toMillisPart() / 10);
+            case OneThousand -> String.format("Timer: %ds", passed.toSeconds());
+            case OneHundred -> String.format("Timer: %d.%ds", passed.toSeconds(), passed.toMillisPart() / 100);
+            case Ten -> String.format("Timer: %d.%02ds", passed.toSeconds(), passed.toMillisPart() / 10);
         };
         setText(scoreText);
     }
