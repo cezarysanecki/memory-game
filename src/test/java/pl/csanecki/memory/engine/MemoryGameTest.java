900b3ddd3cfbd23e151d59c15f2e2489f07b5a88
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-04-15 23:49:25 +0200

    fixed tests

--- src/test/java/pl/csanecki/memory/engine/MemoryGameTest.java
+++ src/test/java/pl/csanecki/memory/engine/MemoryGameTest.java
@@ -1,118 +1,155 @@
 package pl.csanecki.memory.engine;
 
 import org.junit.jupiter.api.Test;
+import pl.csanecki.memory.engine.state.FlatItemCurrentState;
+import pl.csanecki.memory.engine.state.GroupOfFlatItemsCurrentState;
+import pl.csanecki.memory.engine.state.MemoryGameCurrentState;
 
+import java.util.Collection;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.csanecki.memory.engine.GuessResult.*;
 
 class MemoryGameTest {
 
-    FlatItemsGroupId firstFlatItemsGroupId = FlatItemsGroupId.of(0);
-    FlatItemsGroupId secondFlatItemsGroupId = FlatItemsGroupId.of(1);
-
-    FlatItemId firstFlatItemId = FlatItemId.of(1);
-    FlatItemId secondFlatItemId = FlatItemId.of(2);
-    FlatItemId thirdFlatItemId = FlatItemId.of(3);
-    FlatItemId fourthFlatItemId = FlatItemId.of(4);
-    FlatItemId fifthFlatItemId = FlatItemId.of(5);
-
     @Test
-    void properly_guessed_cards() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
+    void turning_first_card_from_group_informs_to_continue_guessing() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
 
-        GuessResult firstGuess = memoryGame.turnCard(firstFlatItemId);
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        FlatItemCurrentState firstFlatItemOfFirstGroup = getFirstFlatItem(firstGroupOfFlatItems);
 
-        assertEquals(Continue, firstGuess);
-
-        GuessResult secondGuess = memoryGame.turnCard(secondFlatItemId);
-
-        assertEquals(Guessed, secondGuess);
-    }
-
-    @Test
-    void need_to_reveal_all_cards_from_group() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId, fifthFlatItemId)))));
-
-        memoryGame.turnCard(thirdFlatItemId);
-        GuessResult result = memoryGame.turnCard(fourthFlatItemId);
+        GuessResult result = memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
 
         assertEquals(Continue, result);
     }
 
     @Test
-    void fail_to_guess_all_cards_from_group() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
+    void guessing_all_cards_in_group_requires_turning_them_all_in_row() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
 
-        memoryGame.turnCard(thirdFlatItemId);
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        Set<FlatItemCurrentState> flatItems = firstGroupOfFlatItems.flatItems();
 
-        GuessResult result = memoryGame.turnCard(firstFlatItemId);
+        flatItems.stream()
+                .skip(1)
+                .forEach(flatItem -> memoryGame.turnCard(flatItem.flatItemId()));
+
+        FlatItemCurrentState firstFlatItemOfFirstGroup = getFirstFlatItem(firstGroupOfFlatItems);
+        GuessResult result = memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
+
+        assertEquals(Guessed, result);
+    }
+
+    @Test
+    void not_guessing_all_cards_in_group_in_row_determines_failure() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
+
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        FlatItemCurrentState firstFlatItemOfFirstGroup = getFirstFlatItem(firstGroupOfFlatItems);
+        memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
+
+        GroupOfFlatItemsCurrentState secondGroupOfFlatItems = getSecondGroup(currentState);
+        FlatItemCurrentState secondFlatItemOfFirstGroup = getFirstFlatItem(secondGroupOfFlatItems);
+        GuessResult result = memoryGame.turnCard(secondFlatItemOfFirstGroup.flatItemId());
 
         assertEquals(Failure, result);
     }
 
     @Test
-    void reset_revealed_cars_after_wrong_guess() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
+    void next_turn_after_failure_informs_to_continue_guessing() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
 
-        memoryGame.turnCard(firstFlatItemId);
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        FlatItemCurrentState firstFlatItemOfFirstGroup = getFirstFlatItem(firstGroupOfFlatItems);
+        memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
 
-        memoryGame.turnCard(thirdFlatItemId);
+        GroupOfFlatItemsCurrentState secondGroupOfFlatItems = getSecondGroup(currentState);
+        FlatItemCurrentState secondFlatItemOfFirstGroup = getFirstFlatItem(secondGroupOfFlatItems);
+        memoryGame.turnCard(secondFlatItemOfFirstGroup.flatItemId());
 
-        GuessResult result = memoryGame.turnCard(secondFlatItemId);
-
-        assertEquals(Continue, result);
-    }
-
-    @Test
-    void can_continue_to_guessing_next_cards() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
-
-        memoryGame.turnCard(firstFlatItemId);
-        memoryGame.turnCard(secondFlatItemId);
-
-        GuessResult result = memoryGame.turnCard(thirdFlatItemId);
+        GuessResult result = memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
 
         assertEquals(Continue, result);
     }
 
     @Test
-    void end_game() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
+    void next_turn_after_guess_one_group_informs_to_continue_guessing() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
 
-        memoryGame.turnCard(firstFlatItemId);
-        memoryGame.turnCard(secondFlatItemId);
-        memoryGame.turnCard(thirdFlatItemId);
-        GuessResult result = memoryGame.turnCard(fourthFlatItemId);
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        firstGroupOfFlatItems.flatItems()
+                .forEach(flatItem -> memoryGame.turnCard(flatItem.flatItemId()));
 
-        assertEquals(GameOver, result);
+        GroupOfFlatItemsCurrentState secondGroupOfFlatItems = getSecondGroup(currentState);
+        FlatItemCurrentState secondFlatItemOfFirstGroup = getFirstFlatItem(secondGroupOfFlatItems);
+        GuessResult result = memoryGame.turnCard(secondFlatItemOfFirstGroup.flatItemId());
+
+        assertEquals(Continue, result);
     }
 
     @Test
-    void guess_cards_are_not_counting_as_mistake() {
-        MemoryGame memoryGame = new MemoryGame(new MemoryGameSetup(Set.of(
-                new GroupToGuess(firstFlatItemsGroupId, Set.of(firstFlatItemId, secondFlatItemId)),
-                new GroupToGuess(secondFlatItemsGroupId, Set.of(thirdFlatItemId, fourthFlatItemId)))));
+    void guess_all_groups_ends_game() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
 
-        memoryGame.turnCard(firstFlatItemId);
-        memoryGame.turnCard(secondFlatItemId);
-        memoryGame.turnCard(thirdFlatItemId);
-        GuessResult result = memoryGame.turnCard(firstFlatItemId);
+        memoryGame.currentState()
+                .groupOfFlatItems()
+                .stream()
+                .map(GroupOfFlatItemsCurrentState::flatItems)
+                .flatMap(Collection::stream)
+                .forEach(flatItem -> memoryGame.turnCard(flatItem.flatItemId()));
+
+        MemoryGameCurrentState currentState = memoryGame.currentState();
+
+        assertTrue(currentState.isFinished());
+    }
+
+    @Test
+    void guess_cards_are_not_counting_as_mistake_when_try_to_turn_them() {
+        MemoryGame memoryGame = MemoryGame.create(4, 2);
+        MemoryGameCurrentState currentState = memoryGame.currentState();
+
+        GroupOfFlatItemsCurrentState firstGroupOfFlatItems = getFirstGroup(currentState);
+        firstGroupOfFlatItems.flatItems()
+                .forEach(flatItem -> memoryGame.turnCard(flatItem.flatItemId()));
+
+        GroupOfFlatItemsCurrentState secondGroupOfFlatItems = getSecondGroup(currentState);
+        FlatItemCurrentState secondFlatItemOfFirstGroup = getFirstFlatItem(secondGroupOfFlatItems);
+        memoryGame.turnCard(secondFlatItemOfFirstGroup.flatItemId());
+
+        FlatItemCurrentState firstFlatItemOfFirstGroup = getFirstFlatItem(firstGroupOfFlatItems);
+        GuessResult result = memoryGame.turnCard(firstFlatItemOfFirstGroup.flatItemId());
 
         assertEquals(Continue, result);
+    }
+
+    private static GroupOfFlatItemsCurrentState getFirstGroup(MemoryGameCurrentState currentState) {
+        return currentState.groupOfFlatItems()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("should contain at least one group"));
+    }
+
+    private static GroupOfFlatItemsCurrentState getSecondGroup(MemoryGameCurrentState currentState) {
+        return currentState.groupOfFlatItems()
+                .stream()
+                .skip(1)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("should contain at least two groups"));
+    }
+
+    private static FlatItemCurrentState getFirstFlatItem(GroupOfFlatItemsCurrentState groupOfFlatItems) {
+        return groupOfFlatItems.flatItems()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("should contain at least one card in group"));
     }
 
 
