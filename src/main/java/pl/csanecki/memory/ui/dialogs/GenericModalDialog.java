644548e239ca46cf1274f87400413d2f6611a899
Cezary Sanecki <cezary.sanecki@decerto.com>
2023-04-16 22:46:21 +0200

    removed useless code

--- src/main/java/pl/csanecki/memory/ui/dialogs/GenericModalDialog.java
+++ src/main/java/pl/csanecki/memory/ui/dialogs/GenericModalDialog.java
@@ -7,11 +7,6 @@ abstract class GenericModalDialog extends JDialog {
     GenericModalDialog(JFrame owner, String title) {
         super(owner, title, true);
         setVisible(false);
-
-        setSize(owner.getWidth() / 2, owner.getHeight() / 3);
-        setLocation(
-                owner.getLocation().x + (owner.getWidth() - getWidth()) / 2,
-                owner.getLocation().y + (owner.getHeight() - getHeight()) / 2);
         setResizable(false);
     }
 }
